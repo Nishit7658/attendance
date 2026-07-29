17e97373ba8325e74dff85d387b97ffeaa4d6792
nishit panchal <nishit8758@gmail.com>
2026-07-29 17:56:37 +0530

    fix: Exclude static CSS, JS assets and API routes from middleware matcher to prevent infinite refresh loop and unstyled UI

--- middleware.ts
+++ middleware.ts
@@ -79,5 +79,5 @@ export default auth((req) => {
 })
 
 export const config = {
-  matcher: ["/((?!api/|_next/static|_next/image|_next/data|__nextjs.*|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|css|js)$).*)"],
 }
